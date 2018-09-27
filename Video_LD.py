c1f9c166beac5e4d8e14bc6a705536e090b3ce25
Spidy20 <kushalbhavsar58@gmail.com>
2018-09-27 11:01:33 +0530

    New file

--- Video_LD.py
+++ Video_LD.py
@@ -10,21 +10,14 @@ while True:
     img_arr = np.array(bytearray(img_resp.read()), dtype=np.uint8)
     img = cv2.imdecode(img_arr, -1)
 
-
-    # orig_frame=img.read()
-
-    # if not ret:
-    #     mobile_video = "http://192.168.1.101:8080/shot.jpg"
-    #     continue
-    frame=cv2.GaussianBlur(img,(5,5),0)
-    hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    low_yellow=np.array([30,90,110])
-    up_yellow=np.array([35, 255, 255])
+    frame = cv2.GaussianBlur(img, (5, 5), 0)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    low_yellow = np.array([10,90, 110])
+    up_yellow = np.array([30, 255, 255])
     mask = cv2.inRange(hsv, low_yellow, up_yellow)
     edges = cv2.Canny(mask, 75, 150)
 
-
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=50)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=40)
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
@@ -37,4 +30,3 @@ while True:
         break
 img.release()
 cv2.destroyAllWindows()
-
